7895bc4deb8ebea0e8210183102084f59d81b8f8
soulrete <42328378+soulrete@users.noreply.github.com>
2024-02-19 18:48:39 +0900

    soulrete first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import variables from './variables.module.scss'
 
 export default function Page() {
-  return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+  return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!#@#@#</h1>
 }
