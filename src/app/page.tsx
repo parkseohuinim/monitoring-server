81b529dacec1cefef481c2c24a135a1d1c2725b6
gloomystore <pikchu3333@gmail.com>
2024-02-19 23:41:00 +0900

    feat: Added chart sample

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import variables from './variables.module.scss'
-
 export default function Page() {
-  return <h1 style={{ color: variables.primaryColor }}>Hello, Next.js!</h1>
+  return <h1 className='c-primary'>Hello, Next.js!</h1>
 }
